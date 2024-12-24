e5920ead18ca9547e38d9d16aafa03a3ae386bb3
jackoup <yusuf.fakhry341@gmail.com>
2024-12-24 16:46:13 +0200

    fixed connection with the firebase

--- lib/utils/firebase_init.dart
+++ lib/utils/firebase_init.dart
@@ -1,5 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 
 Future<void> initializeFirebase() async {
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyDBsKEyGL5GoLIHq37ozhECjvQQlyjxofI",
+      authDomain: "evecou-a286d.firebaseapp.com",
+      projectId: "evecou-a286d",
+      storageBucket: "evecou-a286d.firebasestorage.app",
+      messagingSenderId: "808924373972",
+      appId: "1:808924373972:web:6705d79737115b1eff2c41",
+      measurementId: "G-6H9WDLTWCB"
+    ),
+  );
 }
